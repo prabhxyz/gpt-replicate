dbe49e9f92256e479ac44f0a5a670838928146c0
prabhxyz <prabhnannu07@gmail.com>
2024-08-30 02:15:04 -0400

    Fix 'forward' Method

--- bigram_model.py
+++ bigram_model.py
@@ -68,24 +68,24 @@ class BigramLanguageModel(nn.Module):
         logits = self.token_embedding_table(idx)
 
         if targets is not None:
-            loss = None
-        else:
             B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = targets.view(B*T)
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
             loss = F.cross_entropy(logits, targets)
+        else:
+            loss = None
 
         return logits, loss
 
-def generate(self, idx, max_new_tokens):
-    # idx is (B, T) array of indices in the current context
-    for _ in range(max_new_tokens):
-        logits, loss = self(idx)
-        logits = logits[:, -1, :]
-        probs = F.softmax(logits, dim=-1)
-        idx_next = torch.multinomial(probs, num_samples=1)
-        idx = torch.cat([idx, idx_next], dim=1)
-    return idx
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat([idx, idx_next], dim=1)
+        return idx
 
 model = BigramLanguageModel(vocab_size)
 m = model.to(device)
